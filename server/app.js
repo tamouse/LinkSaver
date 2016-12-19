2bb61d019abb44cd8d82a7c7b88e334a420cc24b
Tamara Temple <tamouse@gmail.com>
2016-12-19 07:46:41 -0600

    Create HomeController
    * add bootstrap

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
+var links = require('./app_server/routes/links');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+// app.use(('/links', links));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/app_server/routes/index.js
+++ server/app_server/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var homeController = require('../controllers/HomeController');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homeController.index);
 
 module.exports = router;
